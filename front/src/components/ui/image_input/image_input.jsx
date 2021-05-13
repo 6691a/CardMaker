f98f8654fe802c46342ba0d3bb256279e187e0c2
6691a <6691a@naver.com>
2021-05-13 20:28:00 +0900

    file input loading

--- front/src/components/ui/image_input/image_input.jsx
+++ front/src/components/ui/image_input/image_input.jsx
@@ -1,7 +1,8 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import styles from './image_input.module.css';
 const ImageInput = ({imageUploader, name, onFileChange }) => {
     const inputRef = useRef();
+    const [loading, setLoading] = useState(false);
 
     const onClick = (event) => {
         event.preventDefault();
@@ -9,7 +10,9 @@ const ImageInput = ({imageUploader, name, onFileChange }) => {
     };
 
     const onChange = async (event) => {
+        setLoading(true);
         const uploaded = await imageUploader.upload(event.target.files[0]);
+        setLoading(false);
         onFileChange({
             name: 'fileName',
             url: '',
@@ -19,7 +22,17 @@ const ImageInput = ({imageUploader, name, onFileChange }) => {
     return (
         <div className={styles.container}>
             <input className={styles.input} onChange={onChange} ref={inputRef} type="file" accept="image/*" name="file" />
-            <button className={styles.button} onClick={onClick}>{name || 'No file'}</button>
+            {
+                !loading && 
+                <button className={`${styles.button} ${name ? styles.pink : styles.grey}`} onClick={onClick}>{name || 'No file'}</button>
+
+            }
+            {
+                loading &&  
+                <div className={styles.loading}>
+                
+                </div>
+            }               
         </div>
     ) 
 };
