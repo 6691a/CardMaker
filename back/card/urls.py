fe7931159e4cabda967e20bf9206e07cf5d13f20
6691a <6691a@naver.com>
2021-05-22 19:30:42 +0900

    card add front, bakc

--- back/card/urls.py
+++ back/card/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import Card_View
+from .views import Card_View, Card_Delete_View
 
 app_name='card_maker'
 
 urlpatterns = [
     path('', Card_View.as_view()),
+    path('delete/', Card_Delete_View.as_view()),
+   
 
 ]
--- back/card/views.py
+++ back/card/views.py
@@ -7,27 +7,53 @@ from rest_framework import status
 from .models import Card
 from .serializer import CardSerializer
 
+def getAuthor(request):
+    return Token.objects.get(key=request.auth.key).user
+
 class Card_View(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
       
-        user = Token.objects.get(key=request.auth.key).user
+        author = getAuthor(request)
 
-        serializer = CardSerializer(Card.objects.filter(author=user), many=True)
+        serializer = CardSerializer(Card.objects.filter(author=author), many=True)
 
         if serializer:
-            print(serializer.data)
             return Response(serializer.data ,status=status.HTTP_200_OK)
        
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-    # def post(self, request):
-    #     pass
+    def post(self, request):
+        author = getAuthor(request)
+        card = request.data.get('card')
 
-    # # def update(self, request):
-    # #     pass
-    
-    # def delete(self, request):
-    #     pass
+        # if card is None:
+        # serializer = CardSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     print(serializer)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
+   
+# class Card_Update_View(APIView):
+
+class Card_Delete_View(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request):
+
+        author = getAuthor(request)
+        cards = request.data.get('id')
+ 
+        if cards is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        Card.objects.filter(pk=cards, author=author).delete()
+
+      
+        return Response(status=status.HTTP_200_OK)
+
+
 
